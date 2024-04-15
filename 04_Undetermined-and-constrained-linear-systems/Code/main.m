b439c0cf3af298f67da98df41570eec1f33d90a8
Sergiusz Warga <s.warga@satrev.space>
2024-04-15 15:11:55 +0200

    feat: develop list 2

--- 04_Undetermined-and-constrained-linear-systems/Code/main.m
+++ 04_Undetermined-and-constrained-linear-systems/Code/main.m
@@ -9,13 +9,15 @@ b = [10;
      -5;
      20];
 
-p = 0.5;
-lambda = 1;
+p = 0.1;
+lambda = 10^-8;
 
-focuss(A, b, p, lambda)
+x = focuss(A, b, p, lambda)
+
+b - A*x
 
 %% Functions
-function focuss(A, b, p, l)
+function x = focuss(A, b, p, l)
 % Apply the FOCal Undetermined System Solver.
 %   Arguments:
 %     A --- Coefficient matrix.
@@ -25,16 +27,27 @@ function focuss(A, b, p, l)
 
 % Affine Scaling Transformation (AST)
 
+if p > 1 || p <= 0
+     error("p has to be in range (0, 1]")
+end
+
 [m, ~] = size(A);
 [~, n] = size(b);
 
 x = rand(m,n)
 
-N = 3;
+N = 10;
 
 for k = 1:N - 1
     W = diag( abs(x).^(1-p/2) )
-    x = W^2 * A' * inv(A * W^2 * A' + l*eye(m)) * b
+%     x = W^2 * A' * inv(A * W^2 * A' + l*eye(m)) * b
+     % Based on Zdunek
+  x_bar = (A*W)' * inv( A*W * (A*W)' + l * eye(m) ) * b
+  x = W*x_bar
 end
 
 end
+
+% The best way for implementation
+% A_bar = A* W
+% A_bar^T * (A_bar * A_bar^T * + lambda * I)^-1 * b
