e1e57db8fd91fee530cb53e0a217b766ea915b97
Sergiusz Warga <s.warga@satrev.space>
2024-04-17 14:19:28 +0200

    feat: introduce list 5

---
+++ 05_Iterative-linear-solvers/Code/main.m
@@ -0,0 +1,65 @@
+%% Problem 1
+clear; 
+clc;
+% 2u -  v           = 0
+% -u + 2v -  w      = 0
+%      -v + 2w -  z = 0
+%           -w + 2z = 5
+A = [ 2, -1,  0,  0;
+     -1,  2, -1,  0;
+      0, -1,  2, -1;
+      0,  0, -1,  2];
+b = [0;
+     0;
+     0;
+     5];
+x_init = zeros(size(A,2), 1);
+[x, r_err] = gaussSeidel(A, b, x_init, 50, 1e-6, A\b);
+
+plot(r_err)
+xlabel("Iteration")
+ylabel("Residual error")
+
+%% Problem 2
+clear all;
+clc;
+A = [1, 1, 1;
+     1, 1, 2;
+     1, 2, 2];
+b = [1;2;1];
+
+x_init = zeros(size(A,2), 1);
+
+[x, r_err] = landweber(A, b, x_init, 50, 1e-6, A\b, 10);
+
+figure
+plot(r_err)
+title("Landweber")
+xlabel("Iteration")
+ylabel("Residual error")
+
+return
+
+[x, r_err] = jacobi(A, b, x_init, 50, 1e-6, A\b);
+
+figure
+plot(r_err)
+title("Jacobi")
+xlabel("Iteration")
+ylabel("Residual error")
+
+[x, r_err] = gaussSeidel(A, b, x_init, 50, 1e-6, A\b);
+
+figure
+plot(r_err)
+title("Gauss-Seidel")
+xlabel("Iteration")
+ylabel("Residual error")
+
+[x, r_err] = sor(A, b, x_init, 50, 1e-6, A\b, 0.1);
+
+figure
+plot(r_err)
+title("SOR")
+xlabel("Iteration")
+ylabel("Residual error")
---
+++ 05_Iterative-linear-solvers/Code/sor.m
@@ -0,0 +1,31 @@
+function [x, r_err] = sor(A, b, initialGuess, maxIterations, tolerance, x_real, omega)
+% Input:
+%   A: Coefficient matrix of the linear equation system
+%   b: Right-hand side vector of the linear equation system
+%   initialGuess: Initial guess for the solution vector
+%   maxIterations: Maximum number of iterations
+%   tolerance: Error tolerance for convergence
+
+n = length(b);
+x = initialGuess;
+r_err = zeros(n,1);
+S = tril(A) + diag(diag(A)) ./ omega;
+T = S - A
+G = inv(S)*T
+
+for k = 1:maxIterations
+    x_prev = x;
+    r_err(k) = norm(x_real - x)/n;
+    x = inv(S)*(T*x_prev + b);
+
+    if r_err(k) < tolerance
+        disp(sprintf('Converged successfully after %d iterations.', k));
+        break;
+    end
+end
+
+if k == maxIterations
+    disp('Maximum number of iterations reached.');
+end
+
+end
