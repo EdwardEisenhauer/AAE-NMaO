b4449bf299f6262a72b3277e3316a37622b7ff10
Sergiusz Warga <230757@student.pwr.edu.pl>
2024-04-17 04:40:18 +0200

    feat: develop list 3 problem 4

--- 03_Ill-posed-least-squares-problems/Report/problems/Problem_4.m
+++ 03_Ill-posed-least-squares-problems/Report/problems/Problem_4.m
@@ -1,28 +1,42 @@
-A = [1, 50, 18, 10;
-     1, 40, 20, 16;
-     1, 35, 14, 10;
-     1, 40, 12, 12;
-     1, 30, 16, 14];
-b = [28;
-     30;
-     21;
-     23;
-     23];
+A = [1, 1;
+     1, 2;
+     1, 3;
+     1, 4;
+     1, 5;
+     1, 6;
+     1, 7;
+     1, 8];
+b = [1.5;
+     2.0;
+     2.8;
+     4.1;
+     4.9;
+     6.3;
+     5.0;
+    11.5];
 a = normal_approximation(A, b)
-solution_error = a - A\b
+normal_approximation_error = a - A\b
+pairs = [1, 1.5; 2, 2; 3, 2.8; 4, 4.1; 5, 4.9; 6, 6.3; 7, 5; 8, 11.5];
+a = linear_regression(pairs(:,1), pairs(:,2))
+linear_regression_error = sum((pairs(:,2)-a(1)-a(2).*pairs(:,1)).^2)
 
 a =
 
-   -5.1890
-    0.3384
-    0.5128
-    0.7085
+   -0.3964
+    1.1464
 
-solution_error =
+normal_approximation_error =
 
-   1.0e-12 *
+   1.0e-14 *
 
-    0.1696
-         0
-   -0.0028
-    0.0111
+    0.3442
+   -0.0222
+
+a =
+
+   -0.3964
+    1.1464
+
+linear_regression_error =
+
+    15.1982
