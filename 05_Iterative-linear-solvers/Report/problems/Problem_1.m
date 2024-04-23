e56362478b92c20eca8285235041d94cff6c96a9
Sergiusz Warga <230757@student.pwr.edu.pl>
2024-04-23 23:30:09 +0200

    feat: develop list 1 problem 1

---
+++ 05_Iterative-linear-solvers/Report/problems/Problem_1.m
@@ -0,0 +1,53 @@
+function [x, r_err] = landweber(A, b, x_init, maxIterations, tolerance, x_real, alpha)
+% LANDWEBER The Landweber iterative method.
+%
+%   x = LANDWEBER(A, b) approximates a solution to the system A*x=b.
+%
+%   Arguments:
+%     A --- Square coefficient matrix.
+%     b --- Column vector of constant terms.
+%     x_init --- An initial guess of the solution vector.
+%     alpha --- The relaxation parameter.
+%
+% Input:
+%   maxIterations: Maximum number of iterations
+%   tolerance: Error tolerance for convergence
+
+[m, n] = size(A);
+
+if m ~= n
+    error('The coefficient matrix A is not square!')
+end
+
+if det(A) < eps
+    error('The coefficient matrix A is singular!')
+end
+
+if length(b) ~= n
+    error('Vector b has wrong length!')
+end
+
+if alpha < 0
+    error('Alpha cannot be smaller than 0!')
+end
+
+x = x_init;
+r_err = zeros(n,1);
+G = eye(n) - alpha * A' * A
+
+for k = 1:maxIterations
+    x_prev = x;
+    r_err(k) = norm(x_real - x)/n;
+    x = x_prev + alpha * A' * (b - A*x_prev);
+
+    if r_err(k) < tolerance
+        fprintf('Converged successfully after %d iterations.\n', k);
+        break;
+    end
+end
+
+if k == maxIterations
+    disp('Maximum number of iterations reached.');
+end
+
+end
