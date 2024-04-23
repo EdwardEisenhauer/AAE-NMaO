d903af0eb8d9d9da5f42fa16c0aea545590e3591
Sergiusz Warga <s.warga@satrev.space>
2024-04-23 23:30:09 +0200

    feat: develop list 1 problem 1

--- 05_Iterative-linear-solvers/Code/main.m
+++ 05_Iterative-linear-solvers/Code/main.m
@@ -13,27 +13,41 @@ b = [0;
      0;
      0;
      5];
+n = length(A);
+k = 100;
 % As this is the same system as the one ine the List 1 Problem 1:
 x_exact = A\b;
 % Following the suggestion from the Problem's description
 x_init = zeros(size(A,2), 1);
 % Landweber (gradient descent)
-n = length(A)
-alpha = 0.1
-G = eye(n) - alpha * A' * A
-k = 50
-e_0 = abs(x_init - x_exact)
-e = G^k * e_0
+alpha = 2/norm(eigs(A,1)*(A'*A));
+G_landweber = eye(n) - alpha * (A' * A);
+e_0 = abs(x_init - x_exact);
+e_landweber = norm(G_landweber^k * e_0)
+% Jacobi
+S_jacobi = diag(diag(A));
+G_jacobi = eye(n) - inv(S_jacobi)*A;
+e_jacobi = norm(G_jacobi^k * e_0)
+% Gauss-Seidel
+S_gauss_seidel = tril(A);
+G_gauss_seidel = eye(n) - inv(S_gauss_seidel)*A;
+e_gauss_seidel = norm(G_gauss_seidel^k * e_0)
+% Successive Over-Relaxation (SOR)
+omega = 1;
+S_sor = tril(A,-1) + diag(diag(A))/omega;
+G_sor = eye(n) - inv(S_sor)*A;
+e_sor = norm(G_sor^k * e_0)
 
-results = struct;
-errors = struct;
-[results.landweber, errors.landweber] = landweber(A, b, x_init, k, 1e-6, A\b, alpha)
-[results.jacobi, errors.jacobi] = jacobi(A, b, x_init, k, 1e-6, A\b);
+methods = struct;
+methods.landweber.G = eye(n) - alpha * A' * A;
+methods.landweber.residual_norm = norm(methods.landweber.G^k * abs(x_init - x_exact));
+[methods.landweber.x, methods.landweber.errors] = landweber(A, b, x_init, k, 1e-6, A\b, alpha);
+% [methods.jacobi.x, errors.jacobi] = jacobi(A, b, x_init, k, 1e-6, A\b);
 
-plot(1:k,errors.landweber,1:k,errors.jacobi)
-xlabel("Iteration")
-ylabel("Residual error")
-legend("Landweber", "Jacobi")
+% plot(1:k,errors.landweber,1:k,errors.jacobi)
+% xlabel("Iteration")
+% ylabel("Residual error")
+% legend("Landweber", "Jacobi")
 
 return
 
